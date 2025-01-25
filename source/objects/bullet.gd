997f7e1d789fcef2a317721db574b7109fa37478
bonzer <193195671+bonzerdev@users.noreply.github.com>
2025-01-25 10:42:31 -0500

    added animated bullets

--- source/objects/bullet.gd
+++ source/objects/bullet.gd
@@ -5,10 +5,20 @@ var pos:Vector2
 var rota:float
 var dir:float
 var speed = 200
+var random;
 
 func _ready() -> void:
 	global_position = pos
 	global_rotation = rota
+	random = randi() % 2
+	print(random)
+	match random:
+		0:
+			$AnimatedSprite2D.play("first")
+		1:
+			$AnimatedSprite2D.play("second")
+		2:
+			$AnimatedSprite2D.play("third")
 	
 func _physics_process(delta: float) -> void:
 	velocity = Vector2(speed, 0).rotated(dir)
