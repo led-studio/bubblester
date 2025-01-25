812bf581fc5d6ceaf47c5f0550f0f860af3ad65d
BIGGFLOPAAA <camiloverad08@gmail.com>
2025-01-25 17:11:19 -0500

    Main Menu basic desing

---
+++ source/scenarios/main_menu.gd
@@ -0,0 +1,20 @@
+class_name MainMenu
+extends Control
+
+@onready var play_button: Button = $MarginContainer/HBoxContainer/VBoxContainer/Play_Button as Button
+@onready var credits_button: Button = $MarginContainer/HBoxContainer/VBoxContainer/Credits_Button as Button
+@onready var exit_button: Button = $MarginContainer/HBoxContainer/VBoxContainer/Exit_Button as Button
+@onready var start_level = preload("res://source/scenarios/game.tscn") as PackedScene
+
+func _ready():
+	play_button.button_down.connect(on_play_pressed)
+	exit_button.button_down.connect(on_exit_pressed)
+
+func on_play_pressed() -> void: 
+	get_tree().change_scene_to_packed(start_level)
+
+func on_credits_pressed() -> void: 
+	pass
+
+func on_exit_pressed() -> void: 
+	get_tree().quit()
